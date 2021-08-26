e46685c6ecc75088a7e76d184b57b20da644566e
root <julmasked>
2021-08-26 13:25:37 -0700

    <betty style>

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints quote
- *    * Return: zero on success
- **/
+* main - Prints quote
+*
+* Return: zero on success
+*/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
