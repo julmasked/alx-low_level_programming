31c8103daa72f0bcc7aca454bd2819faecd639b0
root <julmasked>
2021-09-01 09:13:11 -0700

    <bugs>

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -10,18 +9,5 @@ int main(void)
 int i;
 i = 0;
 
-positive_or_negative(i);
-if (i > 0)
-{
-printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-else
-{
-printf("%d is negative\n", i);
-}
 return (0);
 }
