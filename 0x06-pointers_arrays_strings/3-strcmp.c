9007174bfcae6babf043540cf5c1b7771bfb074e
root <julmasked>
2021-09-10 13:29:28 -0700

    <fix this>

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,10 +6,8 @@
  * Return: less than 0 if s1 is less than s2, 0 if they're equal,
  * ore than 0 if s1 is greater than s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
-
 	while (*s1 == *s2)
 	{
 		if (*s1 == '\0')
