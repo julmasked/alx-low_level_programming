02a3dc81fa660c637c9db6e9b41d003787200843
root <julmasked>
2021-09-10 13:34:48 -0700

    <fix this>

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,20 +2,26 @@
 /**
  * _strcmp - compares two strings
  * @s1: first string to compare
- * s2: second string to compare
- * Return: less than 0 if s1 is less than s2, 0 if they're equal,
- * ore than 0 if s1 is greater than s2
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if the 
+ * more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 == '\0')
-		{
-			return (0);
-		}
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
-	return (*s1 - *s2);
+
+	if (s1[i] == s2[i])
+		return (0);
+	return (s1[i] - s2[i]);
 }
+
