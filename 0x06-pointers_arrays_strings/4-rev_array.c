141aa8b7f82a026017908e5ad9e60cd7d977432b
root <julmasked>
2021-09-10 13:26:06 -0700

    <fix this>

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,18 +7,18 @@
  *  @n: number of elements in array
  *  Return: void
  */
-
 void reverse_array(int *a, int n)
 {
 	int half = n / 2;
 	int left, right, i;
 
 	n -= 1;
+
 	for (i = 0; i < half; i++)
 	{
 		left = a[n - i];
 		right = a[i];
 		a[i] = left;
 		a[n - i] = right;
-	}	
-}	
+	}
+}
