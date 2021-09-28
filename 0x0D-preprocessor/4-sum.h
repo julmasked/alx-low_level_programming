23fa5903db3362f5be91a2e40c417c2c34088b6b
root <julmasked>
2021-09-28 12:50:06 -0700

    <like macro>

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ *  Desc: Header file that defines a function-like macro SUM(x, y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+typedef int make_iso_compilers_happy;
