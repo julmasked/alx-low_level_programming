cf66ed47c1f487057a0d02eef79cd33f55d8d546
root <julmasked>
2021-10-21 10:46:16 -0700

    <letter is the variable>

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "stdlib.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX
@@ -20,12 +20,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	o = open
-		(filename, O_RDONLY);
-	r = read 
-		(o, buffer, letters);
-	w = write
-		(STDOUT-FILENO, buffer,r);
+	o = open(filename, O_RDONLY);
+	r = read (o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
 	if (o == -1 || r == -1 || w == -1 ||  w != r)
 	{
